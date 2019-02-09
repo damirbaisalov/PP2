3fe4029c2a1ed52e9a30138dfe9d1670afe6747e
damirbaisalov <baisalovdamir@gmail.com>
2019-02-09 16:18:48 +0600

    adding task3,4 of lab2

---
+++ week 2/task3/task3/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+
+namespace task3
+{
+    class Program
+    {
+
+        static void PrintSpaces(int level)
+        {
+            for (int i= 0;i < level;i++)
+            {
+                Console.Write(" ");
+            }
+        }
+
+        static void Ex4(DirectoryInfo directory, int level)
+        {
+            
+            FileInfo[] files = directory.GetFiles();
+            DirectoryInfo[] directories = directory.GetDirectories();
+
+            foreach (FileInfo file in files)
+            {
+                PrintSpaces(level);
+                Console.WriteLine(file.Name);
+            }
+
+            foreach (DirectoryInfo d in directories)
+            {
+                PrintSpaces(level);
+                Console.WriteLine(d.Name);
+                Ex4(d, level + 1);
+            }
+        }
+            static void Main(string[] args)
+        {
+            DirectoryInfo d = new DirectoryInfo(@"C:\Users\Lenovo\Desktop\string path");
+            Ex4(d,0);
+            Console.ReadKey();
+        }
+       
+    }
+}
---
+++ week 2/task4/task4/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace task4
+{
+    class Program
+    {
+        static void Main()
+        {
+            StreamWriter sw = new StreamWriter(@"C:\Users\Lenovo\Desktop\path\example1.txt");
+            sw.Write(3242);
+            sw.Close();
+            string path =( @"C:\Users\Lenovo\Desktop\path\example1.txt");
+            
+
+            string path1 = (@"C:\Users\Lenovo\Desktop\path1\example1.txt");
+            File.Copy(path,path1);
+            File.Delete(path);
+        
+            sw.Close();
+        }
+    }
+}
